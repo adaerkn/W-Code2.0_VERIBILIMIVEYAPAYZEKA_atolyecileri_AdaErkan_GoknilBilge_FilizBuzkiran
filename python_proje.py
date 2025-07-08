97f6dd410fbd02e3b2279a60b99df18bc39ec7c6
adaerkn <139787106+adaerkn@users.noreply.github.com>
2025-07-08 17:03:01 +0300

    Update python_proje.py

--- python_proje.py
+++ python_proje.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import numpy as np
 import matplotlib.pyplot as plt
