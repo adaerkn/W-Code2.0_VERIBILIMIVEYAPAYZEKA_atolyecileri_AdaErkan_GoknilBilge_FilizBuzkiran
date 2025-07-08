6baaa366a5b19c0fa49379d0d0fdc91dc7f3137f
adaerkn <139787106+adaerkn@users.noreply.github.com>
2025-07-08 17:02:34 +0300

    Update python_proje.py

--- python_proje.py
+++ python_proje.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
-import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
